a05ce6eac3dc4ff64bb855799b5342d9186d2617
Filip Mašić <shoutplenty@gmail.com>
2024-12-31 17:06:32 +0000

    cmios: injected swiss.dol at Y-hold

--- lib/cmios/src/cmios.c
+++ lib/cmios/src/cmios.c
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <gccore.h>
+// inject swiss.dol binary (placed in lib/cmios/data folder) into cmios
+#include "swiss_dol.h"
 #include "console.h"
 
 extern int execFromMem(void* data);
@@ -23,9 +25,31 @@ static void callMIOS(void) {
 	);
 }
 
+// configures DVD drive (needed for disc change and exit)
+static void resetDI(void) {
+	vu32 *pi_cmd = (u32*) 0xCC003024;
+	vu32 *ios_di_sync = (u32*) 0x800030F8;
+	vu32 *ios_sync_base = (u32*) 0x800030E0;
+	vu32 *phys_ios_di_sync = (u32*) 0xC00030F8;
+
+	*pi_cmd |= 0b101; // GameCube hardware registers: not DI reset, MEM reset, SYS reset rsp.
+	// SYS reset avoids crash and not DI reset avoids "detecting devices" double drive spin-up in Swiss
+	*ios_di_sync = 1; // DI reset compatibility bit
+	DCFlushRange((void *) ios_sync_base, 32);
+	while (*phys_ios_di_sync != 0); // wait for Starlet
+}
+
 int main(int argc, char **argv) {
 	char* legacyMagic = (char*)0x807fffe0;
     consoleInit(); // some sort of video init is *required* by hardware for input detection
+
+	// launch bundled homebrew if Y-hold detected
+	if (padScanOnNextFrame() && (PAD_ButtonsHeld(0) & PAD_BUTTON_Y)) {
+		// branch to payload (in cmios binary)
+		resetDI(); // needed with current Swiss to prevent double drive restart
+		execFromMem((void*)swiss_dol);
+		sleep(3);
+	} else
 
 	// old cmios behaviour
 	if (strncmp(legacyMagic, "gchomebrew dol", 32) == 0) {
